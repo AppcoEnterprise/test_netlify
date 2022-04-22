4510525b41be0a2089a41fe00dfe1449f1953f4d
Chhann Chikay <appco.enterprise.2021@gmail.com>
2022-04-22 16:35:49 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -20,9 +20,9 @@ const express = require("express");
 const serverless = require("serverless-http");
 
 const app = express();
-const router = express.Router();
+// const router = express.Router();
 
-router.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.json({
         hello: "hi!",
     });
