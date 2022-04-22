1ddc7067a41f642649880339df326e356760c54e
Chhann Chikay <appco.enterprise.2021@gmail.com>
2022-04-22 16:44:48 +0700

    x

--- src/api.js
+++ src/api.js
@@ -20,15 +20,15 @@ const express = require("express");
 const serverless = require("serverless-http");
 
 const app = express();
-const router = express.Router();
+// const router = express.Router();
 
-router.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.json({
         hello: "hi!",
     });
 });
 
-app.use('/.netlify/functions/api', router);
+// app.use('/.netlify/functions/api', router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
