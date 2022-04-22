19d7cbe0152194341056b9b2101734c54f709bb4
Chhann Chikay <appco.enterprise.2021@gmail.com>
2022-04-22 17:05:10 +0700

    q

--- src/api.js
+++ src/api.js
@@ -28,7 +28,7 @@ router.get("/", (req, res) => {
     });
 });
 
-app.use('/.netlify/functions/index.js', router);
+app.use('/.netlify/functions/api.js', router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
